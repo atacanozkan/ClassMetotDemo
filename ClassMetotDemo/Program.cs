d97232509a8e6b2dfea6405d17b291f99e20e3c2
Atacan Ozkan <atacanozkan@gmail.com>
2022-06-06 22:44:44 +0300

    Customer and CutomerManager classes added.

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,21 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Atacan";
+            customer1.Surname = "Özkan";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 1;
+            customer2.Name = "Ali";
+            customer2.Surname = "Özkan";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Update(customer1);
+            customerManager.Monitor(customer2);
         }
     }
 }
